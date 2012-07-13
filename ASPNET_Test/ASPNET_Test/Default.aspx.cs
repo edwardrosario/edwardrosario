3636430d96eed445bba3c357b8695231034b9c74
Edward Rosario <edwardrosariosr@yahoo.com>
2012-07-12 20:10:08 -0700

    first repo

---
+++ ASPNET_Test/ASPNET_Test/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPNET_Test
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Session["test2"] = "Hello World";
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Button1.Text = Session["test2"].ToString();
+        }
+    }
+}
---
+++ ASPNET_Test/ASPNET_Test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASPNET_Test.Global" Language="C#" %>
